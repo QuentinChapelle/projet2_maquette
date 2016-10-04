f790ff696e739fb544887846fd1e00c64824d801
Quentin <chapellequentin@live.fr>
2016-10-04 17:41:42 +0200

    first commit

---
+++ formulaire.php
@@ -0,0 +1,26 @@
+<div style="width: 30%; margin: auto;">
+
+		<form>
+		  <div class="form-group">
+		    <label for="email">Votre mail :</label>
+		    <input type="email" class="form-control" id="email" placeholder="Ecrivez votre mail">
+		  </div>
+		  <div class="form-group">
+		    <label for="text">Votre prénom :</label>
+		    <input type="text" class="form-control" id="prenom" placeholder="Ecrivez votre prénom">
+		  </div>
+		  <div class="form-group">
+		    <label for="text">Votre numéro :</label>
+		    <input type="text" class="form-control" id="numéro" placeholder="Ecrivez votre numéro">
+		  </div>
+		  <div class="form-group">
+		  <label for="comment">Votre message :</label>
+		  <textarea class="form-control" rows="5" id="message" placeholder="Ecrivez votre message"></textarea>
+		</div>
+		<label class="control-label">Envoyer une photos</label>
+	<input id="input-2" name="input2[]" type="file" class="file" multiple data-show-upload="false" data-show-caption="true">
+		  <div style="text-align: center;">
+		 <button style="margin-top: 7px;" type="submit" class="btn btn-default">Envoyer</button>
+		 </div>
+		</form>
+	</div>
